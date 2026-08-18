6cbc076333ba609472f5628c7ee0e47d70a57695
Samuditha Seneviratne <e20369@eng.pdn.ac.lk>
2026-08-18 17:15:15 +0530

    Fix: setup wizard map hight adjusted to min 400px_2

--- frontend/src/views/Setup.jsx
+++ frontend/src/views/Setup.jsx
@@ -202,8 +202,8 @@ export default function Setup({ onComplete }) {
               </div>
             </div>
 
-            <div className="w-full border border-gray-100 dark:border-slate-700 rounded-xl z-0 relative overflow-hidden shadow-sm" style={{ height: '400px' }}>
-              <MapContainer center={[formData.latitude || 0, formData.longitude || 0]} zoom={2} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
+            <div className="w-full border border-gray-100 dark:border-slate-700 rounded-xl z-0 relative overflow-hidden shadow-sm" style={{ height: '500px' }}>
+              <MapContainer center={[formData.latitude || 0, formData.longitude || 0]} zoom={2} scrollWheelZoom={true} style={{ height: '500px', width: '100%' }}>
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                 <LocationMarker
                   position={{ lat: formData.latitude, lng: formData.longitude }}
